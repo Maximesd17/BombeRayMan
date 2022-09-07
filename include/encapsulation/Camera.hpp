d60ef1c90b01868d8c38d53c0a014ff4a3b03eed
Maximesd17 <maximesd.kodoku17@gmail.com>
2022-09-07 15:22:53 +0200

    [ADD] Encapsulation Camera class

---
+++ include/encapsulation/Camera.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "raylib.h"
+#include "Vector3.hpp"
+
+#include <memory>
+
+class BRMCamera
+{
+private:
+    std::unique_ptr<Camera> _camera;
+    int _mode;
+    float _fov;
+    BRMVector3 _position;
+    BRMVector3 _up;
+    BRMVector3 _target;
+public:
+    BRMCamera();
+    void setPosition(BRMVector3 position);
+    void setMode(int mode);
+    void setFOV(float fov);
+    void setUp(BRMVector3 up);
+    void setTarget(BRMVector3 target);
+    Camera *getCamera() const;
+    int getMode() const;
+    float getFOV() const;
+    BRMVector3 getPosition() const;
+    BRMVector3 getUp() const;
+    BRMVector3 getTarget() const;
+    void update() const;
+    ~BRMCamera();
+};
---
+++ src/encapsulation/Camera.cpp
@@ -0,0 +1,84 @@
+#include "encapsulation/Camera.hpp"
+
+BRMCamera::BRMCamera()
+{
+    _camera = std::make_unique<Camera>();
+}
+
+void BRMCamera::setPosition(BRMVector3 position)
+{
+    _position = position;
+    _camera.get()->position.x = _position.getX();
+    _camera.get()->position.y = _position.getY();
+    _camera.get()->position.z = _position.getZ();
+    this->update();
+}
+
+void BRMCamera::setMode(int mode)
+{
+    _mode = mode;
+    _camera.get()->projection = _mode;
+    this->update();
+}
+
+void BRMCamera::setFOV(float fov)
+{
+    _fov = fov;
+    _camera.get()->fovy = _fov;
+    this->update();
+}
+
+void BRMCamera::setUp(BRMVector3 up)
+{
+    _up = up;
+    _camera.get()->up.x = _up.getX();
+    _camera.get()->up.y = _up.getY();
+    _camera.get()->up.z = _up.getZ();
+    this->update();
+}
+
+void BRMCamera::setTarget(BRMVector3 target)
+{
+    _target = target;
+    _camera.get()->target.x = _target.getX();
+    _camera.get()->target.y = _target.getY();
+    _camera.get()->target.z = _target.getZ();
+    this->update();
+}
+
+Camera *BRMCamera::getCamera() const
+{
+    return _camera.get();
+}
+
+int BRMCamera::getMode() const
+{
+    return _mode;
+}
+
+float BRMCamera::getFOV() const
+{
+    return _fov;
+}
+
+BRMVector3 BRMCamera::getPosition() const
+{
+    return _position;
+}
+
+BRMVector3 BRMCamera::getUp() const
+{
+    return _up;
+}
+
+BRMVector3 BRMCamera::getTarget() const
+{
+    return _target;
+}
+
+void BRMCamera::update() const
+{
+    UpdateCamera(_camera.get());
+}
+
+BRMCamera::~BRMCamera() {}
